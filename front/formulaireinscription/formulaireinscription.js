405339cd4caf2a10c4d8e736e19cd5e367482362
cbouyssi <cesar.bouyssi@insa-lyon.fr>
2017-04-18 11:17:27 +0200

    Add files via upload

---
+++ front/formulaireinscription/formulaireinscription.js
@@ -0,0 +1,7 @@
+/*angular.module('BlankApp').component('formulaireinscription', {
+   templateUrl: 'formulaireinscription.html',
+   controller: ['$scope', '$window', function($scope, $window) {
+     $scope.formulaireinscription = function() { console.log("Inscris"); $window.location.href = 'gourmandise.html'; }
+     $scope.titre = "je m'inscris";
+   }]
+})*/
